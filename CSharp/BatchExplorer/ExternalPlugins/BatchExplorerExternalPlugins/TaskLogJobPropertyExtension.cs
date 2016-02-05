6c0c6cc2c7299f60f0801d34cc216b40cd88e5a6
testaccount67 <nirajg@nirajg-dev2.redmond.corp.microsoft.com>
2016-02-05 14:40:35 -0800

    Add a dummy plugin for Nitin to extend

---
+++ CSharp/BatchExplorer/ExternalPlugins/BatchExplorerExternalPlugins/TaskLogJobPropertyExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.Azure.BatchExplorer.Helpers;
+using Microsoft.Azure.BatchExplorer.ViewModels;
+
+namespace Microsoft.Azure.BatchExplorer.Plugins.JobPropertyPlugin
+{
+    [Export(typeof(IJobPropertiesExtension))]
+    [ExportMetadata("Name", "TaskLog")]
+    public class TaskLogJobPropertyExtension : IJobPropertiesExtension
+    {
+        public CommandBase GetCommandBase()
+        {
+            return new CommandBase(null);
+        }
+    }
+}
